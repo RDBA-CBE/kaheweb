17ccae8ec81d63fb1f7d2252e9ea23f0fdbaf44c
Duraisamy303 <psmkduraisamy@gmail.com>
2025-12-05 15:39:14 +0530

    josn

---
+++ JSON/ACADEMIC/Arts/Overview.js
@@ -0,0 +1,16 @@
+const overview = {
+  name: "Dr. Ragavi V,",
+  position: "Dean, Faculty of Arts, Science, Commerce, and Management",
+  content: [
+    "Dr. Ragavi V is a visionary academician with over two decades of experience in higher education. She holds a Ph.D. in Computer Science, an M.Phil., an MCA, and a B.Sc. in Physics, reflecting her strong interdisciplinary foundation spanning science, technology, and holistic education.",
+
+    "Dr. Ragavi, as a transformational leader, pioneered initiatives such as 24×7 student helplines, open forums, inclusive student clubs, and leadership development programs—promoting a student-centered environment that fosters empowerment, experiential learning, and emotional well-being.",
+
+    "Her research interests include Artificial Intelligence, Healthcare Technologies, Smart Systems, and Educational Innovation. With over 15 years of active research contribution, she has published extensively in Scopus and Web of Science-indexed journals, filed multiple patents, and secured more than ₹1.26 crores in competitive research grants from DST and other national agencies.",
+
+    "Her academic ethos is rooted in innovation, inclusivity, and student empowerment. Through her leadership and compassionate mentorship, she continues to shape the next generation of scholars and professionals, making a lasting impact on higher education.",
+  ],
+  img: "https://kahedu.edu.in/n/wp-content/uploads/2025/09/Ragavi.jpg",
+};
+
+
