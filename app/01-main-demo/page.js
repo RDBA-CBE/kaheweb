24a13ba0c75c4b39ff1fc9e1b2c2589cf9153053
IndumathiNavinkumar <rdba.developer@gmail.com>
2026-01-05 17:13:42 +0530

    page tile and color update

--- app/01-main-demo/page.js
+++ app/01-main-demo/page.js
@@ -2,8 +2,8 @@ import HomePageLayout from "./(main-demo)";
 import { getAllPostsMeta } from "@/mdx";
 
 export const metadata = {
-  title: "Histudy - Online Courses & Education NEXTJS14 Template",
-  description: "Online Courses & Education NEXTJS14 Template",
+  title: "Karpagam Academy of Higher Education - Deemed University",
+  description: "Karpagam Academy of Higher Education (KAHE) offers a wide range of UG, PG &amp; research programs in Engineering, Arts &amp; Science, Management, Architecture, and Pharmacy.",
 };
 
 const HomePage = async () => {
--- app/kahe/administration/deans/page.jsx
+++ app/kahe/administration/deans/page.jsx
@@ -1,8 +1,8 @@
 import Dean from "@/components/00-Kahe/AdministrationComponents.jsx/Dean";
 
 export const metadata = {
-  title: "Histudy - Online Courses & Education NEXTJS14 Template",
-  description: "Online Courses & Education NEXTJS14 Template",
+  title: "Karpagam Academy of Higher Education - Deemed University",
+  description: "Karpagam Academy of Higher Education (KAHE) offers a wide range of UG, PG &amp; research programs in Engineering, Arts &amp; Science, Management, Architecture, and Pharmacy.",
 };
 
 const page = async () => {
