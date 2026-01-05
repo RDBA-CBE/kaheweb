6312793fd9eba78ed120af247156933c34718ec1
nsathiyakala <nsathiyakala6@gmail.com>
2026-01-05 18:01:32 +0530

    office of international affairs, iqac

---
+++ app/kahe/office-of-international-affairs/bon-voyage-scholarship/page.jsx
@@ -0,0 +1,15 @@
+import BonVoyageCom from "@/components/00-Kahe/OfficeOfInternationalCompoenets/BonVoyageCom";
+
+export const metadata = {
+  title: "About Us 01 - Online Courses & Education NEXTJS14 Template",
+  description: "Online Courses & Education NEXTJS14 Template",
+};
+const page = () => {
+  return (
+    <>
+      <BonVoyageCom/>
+    </>
+  );
+};
+
+export default page;
