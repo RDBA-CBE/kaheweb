b6c3b613e53b5e6590a6e1b7ffec9f146a5bc78b
nsathiyakala <nsathiyakala6@gmail.com>
2026-01-06 15:11:20 +0530

    sports

---
+++ app/kahe/campus-life/sports/page.jsx
@@ -0,0 +1,14 @@
+
+export const metadata = {
+  title: "About Us 01 - Online Courses & Education NEXTJS14 Template",
+  description: "Online Courses & Education NEXTJS14 Template",
+};
+const page = () => {
+  return (
+    <>
+      sports
+    </>
+  );
+};
+
+export default page;
