01b88581d1e60d313c53368a33111195fda3423c
Duraisamy303 <psmkduraisamy@gmail.com>
2026-01-07 10:20:17 +0530

    foreign language

---
+++ app/kahe/office-of-international-affairs/foreign-languages/page.jsx
@@ -0,0 +1,16 @@
+import ForeignLanguages from "@/components/00-Kahe/OfficeOfInternationalCompoenets/ForeignLanguages";
+import StudentsExProCom from "@/components/00-Kahe/OfficeOfInternationalCompoenets/StudentsExProCom";
+
+export const metadata = {
+  title: "About Us 01 - Online Courses & Education NEXTJS14 Template",
+  description: "Online Courses & Education NEXTJS14 Template",
+};
+const page = () => {
+  return (
+    <>
+      <ForeignLanguages/>
+    </>
+  );
+};
+
+export default page;
--- components/00-Kahe/DirectPageComponent/office-of-international-affairs-overview.jsx
+++ components/00-Kahe/DirectPageComponent/office-of-international-affairs-overview.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import InnerBanner from "../common-components/InnerBanner";
-import data from "../../../JSON/Research/overview.json";
+import data from "../../../JSON/OfficeOfInternationAffairs/overview.json";
 import AboutOverview from "../Components/AboutOverview";
 import StickyBreadcrumb from "../common-components/StickyNavBan";
 import { Provider } from "react-redux";
@@ -16,15 +16,15 @@ const OfficeOfInternationalAffairsOverview = () => {
   const research = data;
   console.log("research", research);
 
-  const breadcrumbItems = [{ label: "Research", href: `/kahe/office-of-international-affairs/office-of-international-affairs-overview/` }];
-
+  const breadcrumbItems = [{ label: "Office of International Affairs", href: `/office-of-international-affairs/` },{label:"Overview",href:"/kahe/office-of-international-affairs/office-of-international-affairs-overview/"}];
+  
   return (
     <>
       <Provider store={Store}>
         <Context>
           <InnerBanner data={research?.bannerCon} />
           <section className="bg-white breadcrumb sticky-top">
-            <StickyBreadcrumb items={breadcrumbItems} />
+            <StickyBreadcrumb items={breadcrumbItems} quickLink={false}/>
           </section>
 
           <AboutOverview data={research?.overview} />
