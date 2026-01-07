e936294ae026afbac7a8416f6d0bab7a86f8018a
Pragathi-29 <pragathisarathi02@gmail.com>
2026-01-07 18:03:54 +0530

    Mechanical

---
+++ app/kahe/academics/engineering/department-of-mechanical-engineering/page.jsx
@@ -0,0 +1,16 @@
+import DepartmentOfMech from "@/components/00-Kahe/AcademicsComponents/Engineering/DepartmentOfMech";
+
+export const metadata = {
+  title: "Best MSc Biochemistry Colleges in Coimbatore",
+  description: "kahedu.edu.in",
+};
+
+const page = () => {
+  return (
+    <>
+     <DepartmentOfMech/>
+    </>
+  );
+};
+
+export default page;
--- app/kahe/academics/engineering/department-of-mechanical-sciences-engineering/page.jsx
+++
@@ -1,8 +0,0 @@
-export default function MechanicalSciencesEngineeringPage() {
-  return (
-    <div>
-      <h1>Department of Mechanical Engineering</h1>
-      <p>Welcome to the Department of Mechanical Engineering.</p>
-    </div>
-  );
-}
