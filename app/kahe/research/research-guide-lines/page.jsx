db6c1e1a4b0a0cc4c2664ebc3aafce38ebb1e375
Duraisamy303 <psmkduraisamy@gmail.com>
2026-01-07 14:25:41 +0530

    research guild lines

---
+++ app/kahe/research/research-guide-lines/page.jsx
@@ -0,0 +1,18 @@
+import FundedResearch from "@/components/00-Kahe/DirectPageComponent/FundedResearch";
+import Patent from "@/components/00-Kahe/DirectPageComponent/Patent";
+import ResearchGuideLines from "@/components/00-Kahe/DirectPageComponent/ResearchGuideLines";
+
+
+export const metadata = {
+  title: "About Us 01 - Online Courses & Education NEXTJS14 Template",
+  description: "Online Courses & Education NEXTJS14 Template",
+};
+const page = () => {
+  return (
+    <>
+      <ResearchGuideLines/>
+    </>
+  );
+};
+
+export default page;
