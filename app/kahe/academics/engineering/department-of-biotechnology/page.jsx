14721aeb79e7bff25fb1e57f634a0036b3e56cc5
IndumathiNavinkumar <rdba.developer@gmail.com>
2026-01-08 16:02:16 +0530

    image, title, description , color update

--- app/kahe/academics/engineering/department-of-biotechnology/page.jsx
+++ app/kahe/academics/engineering/department-of-biotechnology/page.jsx
@@ -1,8 +1,8 @@
 import DepartmentOfBiotechEng from "@/components/00-Kahe/AcademicsComponents/Engineering/DepartmentOfBiotechEng";
 
 export const metadata = {
-  title: "Best MSc Biochemistry Colleges in Coimbatore",
-  description: "kahedu.edu.in",
+  title: "Top 10 Biotechnology Colleges in Coimbatore, Tamilnadu",
+  description: "Are you interested in Biotechnology? KAHE is one of the top 10 biotechnology colleges in Coimbatore. Join KAHE and learn from the top-class educators!",
 };
 
 const page = () => {
--- app/kahe/academics/engineering/department-of-civil-engineering/page.jsx
+++ app/kahe/academics/engineering/department-of-civil-engineering/page.jsx
@@ -1,8 +1,8 @@
 import DepartmentOfCivilEng from "@/components/00-Kahe/AcademicsComponents/Engineering/DepartmentOfCivilEng";
 
 export const metadata = {
-  title: "Best MSc Biochemistry Colleges in Coimbatore",
-  description: "kahedu.edu.in",
+  title: "Top Ph.D. Civil Engineering College In Coimbatore<",
+  description: "KAHE is the top Ph.D. civil engineering college in Coimbatore. Our faculty are experienced and give students the knowledge and skills they need to succeed.",
 };
 
 const page = () => {
