2b0f46e7aca932de5d641699b0d9fc57bb3fe683
Duraisamy303 <psmkduraisamy@gmail.com>
2026-01-08 13:05:03 +0530

    adminssion json mega menu admission detail page

--- app/kahe/admissions/admission-details/page.jsx
+++ app/kahe/admissions/admission-details/page.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import AdmissionDetail from "@/components/00-Kahe/DirectPageComponent/AdmissionDetail";
+import ExamNotification from "@/components/00-Kahe/ExaminationComponents/ExamNotification";
 
+export const metadata = {
+  title: "About Us 01 - Online Courses & Education NEXTJS14 Template",
+  description: "Online Courses & Education NEXTJS14 Template",
+};
 const page = () => {
   return (
-    <div>page</div>
-  )
-}
+    <>
+      <AdmissionDetail  />
+    </>
+  );
+};
 
-export default page
+export default page;
