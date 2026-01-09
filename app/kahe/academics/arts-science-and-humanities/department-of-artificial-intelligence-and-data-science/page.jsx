b06070f07951a22777227c32cf91d9bae5dfa97e
Pragathi-29 <148082025+Pragathi-29@users.noreply.github.com>
2026-01-09 22:12:16 +0530

    AI And DS FASCM

---
+++ app/kahe/academics/arts-science-and-humanities/department-of-artificial-intelligence-and-data-science/page.jsx
@@ -0,0 +1,16 @@
+import DepartmentOfArtificialIntelligence from "@/components/00-Kahe/AcademicsComponents/Arts/DepartmentOfArtificialIntelligence.jsx";
+
+
+export const metadata = {
+  title: "About Us 01 - Online Courses & Education NEXTJS14 Template",
+  description: "Online Courses & Education NEXTJS14 Template",
+};
+const page = () => {
+  return (
+    <>
+      <DepartmentOfArtificialIntelligence/>
+    </>
+  );
+};
+
+export default page;
