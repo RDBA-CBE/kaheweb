7f08a1618905800180b5ccaa794b33ca83ce79e5
Duraisamy303 <psmkduraisamy@gmail.com>
2026-01-09 15:49:18 +0530

    nep overview page

---
+++ app/kahe/academics/nep/overview/page.jsx
@@ -0,0 +1,15 @@
+import CIATestCom from "@/components/00-Kahe/ExaminationComponents/CIATestCom";
+
+export const metadata = {
+  title: "CIA Test | Top 10 Engineering Colleges in Coimbatore",
+  description: "Never miss a test again! Quickly and easily check all the exams conducted by KAHE, one of the Top 10 Engineering Colleges in Coimbatore. Start planning ahead.",
+};
+const page = () => {
+  return (
+    <>
+      <CIATestCom overview={true} />
+    </>
+  );
+};
+
+export default page;
