943fc2974660889f96d5187111bf9f02f8074a92
Duraisamy <duraisamy.p@sanorac.com>
2026-01-09 21:56:27 +0530

    report page updated

---
+++ app/kahe/academics/reports/page.jsx
@@ -0,0 +1,17 @@
+import DeptOfBioChemPage from "@/components/00-Kahe/AcademicsComponents/Arts/DeptOfBioChemPage";
+import ReportPage from "@/components/00-Kahe/DirectPageComponent/ReportPage";
+
+export const metadata = {
+  title: "Best MSc Biochemistry Colleges in Coimbatore",
+  description: "Looking for MSc Biochemistry colleges in Coimbatore? Choose KAHE which provides students with the knowledge and skills to become successful in the field.",
+};
+
+const page = () => {
+  return (
+    <>
+     <ReportPage/>
+    </>
+  );
+};
+
+export default page;
