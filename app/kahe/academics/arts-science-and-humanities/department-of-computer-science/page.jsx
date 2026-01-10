b5da35531cee507af8bb49f4047d9fa195b27dca
Pragathi-29 <148082025+Pragathi-29@users.noreply.github.com>
2026-01-10 08:51:39 +0530

    CS

--- app/kahe/academics/arts-science-and-humanities/department-of-computer-science/page.jsx
+++ app/kahe/academics/arts-science-and-humanities/department-of-computer-science/page.jsx
@@ -1,4 +1,4 @@
-import BreadCrumb from "@/components/00-Kahe/common-components/BreadCrumb";
+import DepartmentOfComputerScience from "@/components/00-Kahe/AcademicsComponents/Arts/DepartmentOfComputerScience.jsx";
 
 
 export const metadata = {
@@ -8,8 +8,7 @@ export const metadata = {
 const page = () => {
   return (
     <>
-      <BreadCrumb  />
-      <h5>Dept of bio tech</h5>
+      <DepartmentOfComputerScience/>
     </>
   );
 };
