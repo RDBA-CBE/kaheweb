a167a7f9b33e3f946d7c3d98a76b4b2220afc769
Pragathi-29 <148082025+Pragathi-29@users.noreply.github.com>
2026-01-10 07:07:47 +0530

    IT

---
+++ app/kahe/academics/arts-science-and-humanities/department-of-information-technology/page.jsx
@@ -0,0 +1,16 @@
+import DepartmentOfInformationTechnology from "@/components/00-Kahe/AcademicsComponents/Arts/DepartmentOfInformationTechnology.jsx";
+
+
+export const metadata = {
+  title: "About Us 01 - Online Courses & Education NEXTJS14 Template",
+  description: "Online Courses & Education NEXTJS14 Template",
+};
+const page = () => {
+  return (
+    <>
+      <DepartmentOfInformationTechnology/>
+    </>
+  );
+};
+
+export default page;
