8a084bab26e0e4345ccc10e030572a8702f61eb1
Duraisamy303 <psmkduraisamy@gmail.com>
2026-01-10 17:37:24 +0530

    student page updated

--- app/kahe/students/page.jsx
+++ app/kahe/students/page.jsx
@@ -1,7 +1,59 @@
-import React from 'react'
+"use client";
 
-export default function Students() {
+import data from "../../../JSON/student.json";
+import AboutOverview from "../../../components/00-Kahe/Components/AboutOverview";
+import { Provider } from "react-redux";
+import Context from "@/context/Context";
+import Store from "@/redux/store";
+import {
+  OfficeOfInternationalAffairsJson,
+  Research,
+} from "@/JSON/ACADEMIC/Overview";
+import Sidebar2 from "@/components/00-Kahe/common-components/Sidebar2";
+import Commitee from "@/components/00-Kahe/Components/About/Commitee";
+import ImageCard from "@/components/00-Kahe/Components/ImageCard";
+import { FirstLetterUp } from "@/utils/functions.utils";
+
+const OfficeOfInternationalAffairsOverview = () => {
+  const research = data;
+  console.log("research", research);
+
+  const breadcrumbItems = [
+    {
+      label: "Office of International Affairs",
+      href: `/office-of-international-affairs/`,
+    },
+    {
+      label: "Overview",
+      href: "/kahe/office-of-international-affairs/office-of-international-affairs-overview/",
+    },
+  ];
+
   return (
-    <div>Students</div>
-  )
-}
+    <>
+      <Provider store={Store}>
+        <Context>
+          <AboutOverview data={research?.overview} />
+          <section className="section-wid section-bg1">
+            <Commitee data={research?.student_att} />
+            <Commitee data={research?.internal_mark} />
+            <Commitee data={research?.complaint} />
+            <Commitee data={research?.complaint_against} />
+
+
+            
+            <div className=" section-bg1 ">
+                      <div className="decor-ti">Student Grievance Redressal Committee </div>
+              
+          <ImageCard visionItem={research?.vision} missionItem={research?.mission} />
+        </div>
+          </section>
+
+          <Sidebar2 subMenu={OfficeOfInternationalAffairsJson} />
+        </Context>
+      </Provider>
+    </>
+  );
+};
+
+export default OfficeOfInternationalAffairsOverview;
