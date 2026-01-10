7922edf6d0c18b08d28d9ffe5ffb7d8ddea37b01
IndumathiNavinkumar <rdba.developer@gmail.com>
2026-01-10 18:11:59 +0530

    corrections

--- app/kahe/academics/arts-science-and-humanities/department-of-biotechnology/page.jsx
+++ app/kahe/academics/arts-science-and-humanities/department-of-biotechnology/page.jsx
@@ -2,7 +2,7 @@ import DeptOfBioTech from "@/components/00-Kahe/AcademicsComponents/Arts/DeptOfB
 
 export const metadata = {
   title: "Biotechnology | B.Tech Biotechnology colleges in coimbatore",
-  description: "With state-of-the-art facilities, experienced faculty, and a rigorous curriculum, KAHE dwells as the best B.Tech Biotechnology College in Coimbatore. Join Today!",
+  description: "Fostering scientific discovery and applied biotechnology, to prepare students for research and industry.",
 };
 
 const page = () => {
