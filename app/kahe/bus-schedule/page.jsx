8a7c4cb93bc08e826def804460595ddd3abc8cc5
Duraisamy303 <psmkduraisamy@gmail.com>
2026-01-10 17:55:32 +0530

    bus shedule udpated

--- app/kahe/bus-schedule/page.jsx
+++ app/kahe/bus-schedule/page.jsx
@@ -1,7 +1,8 @@
+import BusShedule from '@/components/00-Kahe/ExaminationComponents/BusShedule'
 import React from 'react'
 
 export default function BusSchedule() {
   return (
-    <div>Bus Schedule</div>
+    <div><BusShedule /></div>
   )
 }
