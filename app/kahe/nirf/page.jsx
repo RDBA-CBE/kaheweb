b026de22547817ae25ee43e32c080a0d3ca2ab98
nsathiyakala <nsathiyakala6@gmail.com>
2026-01-12 16:39:43 +0530

    correction, nirf page

---
+++ app/kahe/nirf/page.jsx
@@ -0,0 +1,16 @@
+import NIRFPage from "@/components/00-Kahe/DirectPageComponent/NIRFPage";
+
+export const metadata = {
+  title: "NIRF | Best MSc Microbiology College in Coimbatore",
+  description: "Being the Best MSc Microbiology College in Coimbatore, we ensure that our students gain the best education and guidance to succeed in their field.",
+};
+
+const page = () => {
+  return (
+    <>
+     <NIRFPage/>
+    </>
+  );
+};
+
+export default page;
